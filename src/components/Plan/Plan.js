2a893200c2bd713c555735ba51046ed2da5237e8
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-30 23:24:19 +0600

    card3

--- src/components/Plan/Plan.js
+++ src/components/Plan/Plan.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CheckCircleIcon } from '@heroicons/react/solid';
+import { CheckCircleIcon, ArrowRightIcon } from '@heroicons/react/solid';
 
 const Plan = ({ plan }) => {
     const { name, price } = plan;
@@ -15,6 +15,7 @@ const Plan = ({ plan }) => {
                 <p className="bg-lime-300 text-gray-700 p-1 rounded text-left flex items-center"><CheckCircleIcon className="w-4 h-4 text-emerald-500 mr-2"></CheckCircleIcon>Awesome</p>
                 <p className="bg-lime-300 text-gray-700 p-1 rounded text-left flex items-center"><CheckCircleIcon className="w-4 h-4 text-emerald-500 mr-2"></CheckCircleIcon>Awesome</p>
                 <p className="bg-lime-300 text-gray-700 p-1 rounded text-left flex items-center"><CheckCircleIcon className="w-4 h-4 text-emerald-500 mr-2"></CheckCircleIcon>Awesome</p>
+                <button className="bg-green-300 text-bold text-gray-600 hover:text-green-700 mt-2 py-1 rounded w-full flex justify-center">Buy Now<ArrowRightIcon className="w-6 h-6 ml-2"></ArrowRightIcon></button>
             </div>
         </div>
     );
